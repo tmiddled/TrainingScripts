6cb7e78905b3184e90d02c7d11b4381c67da5586
tmiddled <tmiddled@gmail.com>
2014-06-04 09:39:51 +1000

    Add UpdateAll

--- WebApplication1/WebApplication1/UpdateAll.js
+++ WebApplication1/WebApplication1/UpdateAll.js
@@ -1,22 +1,19 @@
 ﻿try {
-    //Get field values
-    var primaryrepfrom = VV.Form.GetFieldValue('PrimaryRepFrom');
-    var primaryrepto = VV.Form.GetFieldValue('PrimaryRepTo');
-
     //Construct URL
     var custDbAlias = VV.CustomerDatabaseAlias;
     var custAlias = VV.CustomerAlias;
     var apiVer = "v1";
     var templateId = 'f9c80aec-2ac5-e311-accd-005056805d2b';
 
+    var fields = 'PrimaryRep';
+    var limit = 1000;
+    var offset = 0;
 
-    var fields = 'PrimaryRep,Datafield8'
-    var queryString = '[PrimaryRep] =\'' + primaryrepfrom + '\'';
 
     var existingWorkOrderData = [];
 
 
-    var existingdata = $.getJSON(VV.BaseAppUrl + 'api/' + apiVer + '/' + custAlias + '/' + custDbAlias + '/formtemplates/' + templateId + '/forms?q=' + queryString + '&fields=' + fields)
+    var existingdata = $.getJSON(VV.BaseAppUrl + 'api/' + apiVer + '/' + custAlias + '/' + custDbAlias + '/formtemplates/' + templateId + '/forms?fields=' + fields + '&limit=' + limit + '&offset=' + offset)
     .done(function (result) {
         var numOfForms = result.data.length;
         if (numOfForms > 0) {
@@ -25,12 +22,15 @@
                     formid: result.data[a].instanceName,
                     revisionid: result.data[a].revisionId,
                     currentprimaryrep: result.data[a].primaryRep,
-                    newprimaryrep: primaryrepto
                 });
             }
             var postData = {};
             for (var b = 0; b < numOfForms; b++) {
-                postData.PrimaryRep = existingWorkOrderData[b].newprimaryrep;
+                var lowerCaseRep = existingWorkOrderData[b].currentprimaryrep;
+                lowerCaseRep = lowerCaseRep.toLowerCase();
+                lowerCaseRep = lowerCaseRep.replace(" ", ".");
+                var newvar = 'five';
+                postData.PrimaryRep = lowerCaseRep;
                 var currentFormInstance = existingWorkOrderData[b].revisionid;
                 //Make Request to Post Data
                 var requestPost = $.ajax({
